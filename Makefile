15312abe5aea81e75a8fd83800980017c50d8696
Egor Blagov <e.m.blagov@gmail.com>
2020-02-03 21:07:28 +0700

    Add distribution support

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 test:
 	python -m unittest
+
+dist:
+	python setup.py sdist
+
+upload-dist:
+	twine upload dist/*
---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='l2address',
+    packages=['l2address'],
+    version='1.0.0',
+    license='MIT',
+    description='MAC address manipulation library',
+    author='Egor Blagov',
+    author_email='e.m.blagov@gmail.com',
+    url='https://github.com/EgorBlagov/l2address',
+    download_url='https://github.com/EgorBlagov/l2address/archive/v1.0.0.tar.gz',
+    keywords=['Networking', 'Telecommunication', 'MAC', 'L2'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
